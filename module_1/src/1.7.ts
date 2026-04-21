9ebbc2b85c6d205033a23a02c7abbfa6760e3cba
sheikh-riyadh <sheikhriyad350883@gmail.com>
2026-04-21 09:21:34 +0600

    learing spread and rest operator in typeScript

--- module_1/src/1.7.ts
+++ module_1/src/1.7.ts
@@ -1,13 +1,40 @@
 {
+  // Lear spread and rest
 
-// Lear spread and rest
+  // Spread Operator
+  const bros1: string[] = ["Polash", "Sakib", "Anik"];
+  const bros2: string[] = ["Akram", "Babu", "Jibon"];
+  bros1.push(...bros2); // Spread o
 
+  const mentors1: {
+    typeScript: string;
+    redux: string;
+    dbms: string;
+  } = {
+    typeScript: "Mezba",
+    redux: "Mir",
+    dbms: "Mizan",
+  };
 
+  const mentors2: {
+    prisma: string;
+    nextJs: string;
+    cloud: string;
+  } = {
+    prisma: "Firoz",
+    nextJs: "Tonmoy",
+    cloud: "Nahid",
+  };
 
+  const mentorlist = {
+    ...mentors1,
+    ...mentors2,
+  };
 
-
-
-
-
-
+  //   Rest Operator
+  const greting = (...friends: string[]): undefined => {
+    friends.forEach((friend) => {
+      console.log(`Hi ${friend}`);
+    });
+  };
 }
