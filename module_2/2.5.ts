792b8256d6f234cb21119925c85ccb9ace688f2c
sheikh-riyadh <sheikhriyad350883@gmail.com>
2026-04-24 11:34:54 +0600

    learning generic function

---
+++ module_2/2.5.ts
@@ -0,0 +1,44 @@
+{
+  // Generic Function
+
+  const createArray = (param: string): string[] => {
+    return [param];
+  };
+
+  const result1 = createArray("Bangladesh");
+
+  const createGenericArray = <T>(param: T): T[] => {
+    return [param];
+  };
+
+  const result2 = createGenericArray<string>("Bangladesh");
+  const result3 = createGenericArray<boolean>(true);
+  const result4 = createGenericArray<{ name: string; roll: number }>({
+    name: "Riyadh",
+    roll: 350883,
+  });
+
+  interface User {
+    name: string;
+    roll: number;
+  }
+
+  const result5 = createGenericArray<User>({
+    name: "Riyadh",
+    roll: 350883,
+  });
+
+  // create tuple with generic
+
+  const createTuple = (param1: string, param2: string): [string, string] => {
+    return [param1, param2];
+  };
+
+  const result6 = createTuple("Riyadh", "Polash");
+
+  const createTupleWithGeneric = <X, Y>(param1: X, param2: Y): [X, Y] => {
+    return [param1, param2];
+  };
+
+  const result7 = createTupleWithGeneric<string, number>("Riyadh", 350883);
+}
